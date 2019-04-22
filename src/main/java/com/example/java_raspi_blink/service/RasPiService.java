0358f7e625750547bfc37b54251ec9ad04403437
Valentin Grégoire <gregoire.valentin@cropdesign.com>
2019-04-22 09:35:13 +0200

    Initial commit

--- src/main/java/com/example/java_raspi_blink/service/RasPiService.java
+++ src/main/java/com/example/java_raspi_blink/service/RasPiService.java
@@ -1,10 +1,24 @@
 package com.example.java_raspi_blink.service;
 
+import com.pi4j.io.gpio.*;
 import org.springframework.stereotype.Service;
 
 @Service
 public class RasPiService {
     public String blink() {
-        return "Blinking!";
+        GpioController gpio = GpioFactory.getInstance();
+        GpioPinDigitalOutput ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_01);
+        String status;
+        if (ledPin.getState().isHigh()) {
+            ledPin.setState(PinState.LOW);
+            status = "off";
+        } else {
+            status = "on";
+            ledPin.setState(PinState.HIGH);
+        }
+        gpio.shutdown();
+        gpio.unprovisionPin(ledPin);
+
+        return "The led is now " + status + "!";
     }
 }
